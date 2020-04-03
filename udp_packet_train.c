b08b142394eef3d6dd0315fd6429b9aee8024efa
queenrhea <earthtravel2010@gmail.com>
2020-04-02 18:13:26 -0700

    Create udp_packet_train.c

---
+++ udp_packet_train.c
@@ -0,0 +1,145 @@
+// IPv4 header
+
+  // IPv4 header length (4 bits): Number of 32-bit words in header = 5
+  iphdr.ip_hl = IP4_HDRLEN / sizeof (uint32_t);
+
+  // Internet Protocol version (4 bits): IPv4
+  iphdr.ip_v = 4;
+
+  // Type of service (8 bits)
+  iphdr.ip_tos = 0;
+
+  // Total length of datagram (16 bits): IP header + UDP header + datalen
+  iphdr.ip_len = htons (IP4_HDRLEN + UDP_HDRLEN + datalen);
+
+  // ID sequence number (16 bits): unused, since single datagram
+  iphdr.ip_id = htons (0);
+
+  // Time-to-Live (8 bits): default to maximum value
+  iphdr.ip_ttl = 255;
+
+  // Transport layer protocol (8 bits): 17 for UDP
+  iphdr.ip_p = IPPROTO_UDP;
+
+  // Source IPv4 address (32 bits)
+  if ((status = inet_pton (AF_INET, src_ip, &(iphdr.ip_src))) != 1) {
+    fprintf (stderr, "inet_pton() failed.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+
+  // Destination IPv4 address (32 bits)
+  if ((status = inet_pton (AF_INET, dst_ip, &(iphdr.ip_dst))) != 1) {
+    fprintf (stderr, "inet_pton() failed.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+
+  // IPv4 header checksum (16 bits): set to 0 when calculating checksum
+  iphdr.ip_sum = 0;
+  iphdr.ip_sum = checksum ((uint16_t *) &iphdr, IP4_HDRLEN);
+
+
+
+
+
+  // UDP header
+
+  // Source port number (16 bits): pick a number
+  udphdr.source = htons (4950);
+
+  // Destination port number (16 bits): pick a number
+  udphdr.dest = htons (4950);
+
+  // Length of UDP datagram (16 bits): UDP header + UDP data
+  udphdr.len = htons (UDP_HDRLEN + datalen);
+
+  // UDP checksum (16 bits)
+  udphdr.check = udp4_checksum (iphdr, udphdr, data, datalen);
+
+  // Fill out ethernet frame header.
+
+  // Ethernet frame length = ethernet data (IP header + UDP header + UDP data)
+  frame_length = IP4_HDRLEN + UDP_HDRLEN + datalen;
+
+  // IPv4 header
+  memcpy (ether_frame, &iphdr, IP4_HDRLEN);
+
+  // UDP header
+  memcpy (ether_frame + IP4_HDRLEN, &udphdr, UDP_HDRLEN);
+
+  // UDP data
+  memcpy (ether_frame + IP4_HDRLEN + UDP_HDRLEN, data, datalen);
+
+
+
+
+
+  // Build IPv4 UDP pseudo-header and call checksum function.
+uint16_t
+udp4_checksum (struct ip iphdr, struct udphdr udphdr, uint8_t *payload, int payloadlen)
+{
+  char buf[IP_MAXPACKET];
+  char *ptr;
+  int chksumlen = 0;
+  int i;
+
+  ptr = &buf[0];  // ptr points to beginning of buffer buf
+
+  // Copy source IP address into buf (32 bits)
+  memcpy (ptr, &iphdr.ip_src.s_addr, sizeof (iphdr.ip_src.s_addr));
+  ptr += sizeof (iphdr.ip_src.s_addr);
+  chksumlen += sizeof (iphdr.ip_src.s_addr);
+
+  // Copy destination IP address into buf (32 bits)
+  memcpy (ptr, &iphdr.ip_dst.s_addr, sizeof (iphdr.ip_dst.s_addr));
+  ptr += sizeof (iphdr.ip_dst.s_addr);
+  chksumlen += sizeof (iphdr.ip_dst.s_addr);
+
+  // Copy zero field to buf (8 bits)
+  *ptr = 0; ptr++;
+  chksumlen += 1;
+
+  // Copy transport layer protocol to buf (8 bits)
+  memcpy (ptr, &iphdr.ip_p, sizeof (iphdr.ip_p));
+  ptr += sizeof (iphdr.ip_p);
+  chksumlen += sizeof (iphdr.ip_p);
+
+  // Copy UDP length to buf (16 bits)
+  memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
+  ptr += sizeof (udphdr.len);
+  chksumlen += sizeof (udphdr.len);
+
+  // Copy UDP source port to buf (16 bits)
+  memcpy (ptr, &udphdr.source, sizeof (udphdr.source));
+  ptr += sizeof (udphdr.source);
+  chksumlen += sizeof (udphdr.source);
+
+  // Copy UDP destination port to buf (16 bits)
+  memcpy (ptr, &udphdr.dest, sizeof (udphdr.dest));
+  ptr += sizeof (udphdr.dest);
+  chksumlen += sizeof (udphdr.dest);
+
+  // Copy UDP length again to buf (16 bits)
+  memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
+  ptr += sizeof (udphdr.len);
+  chksumlen += sizeof (udphdr.len);
+
+  // Copy UDP checksum to buf (16 bits)
+  // Zero, since we don't know it yet
+  *ptr = 0; ptr++;
+  *ptr = 0; ptr++;
+  chksumlen += 2;
+
+  // Copy payload to buf
+  memcpy (ptr, payload, payloadlen);
+  ptr += payloadlen;
+  chksumlen += payloadlen;
+
+  // Pad to the next 16-bit boundary
+  for (i=0; i<payloadlen%2; i++, ptr++) {
+    *ptr = 0;
+    ptr++;
+    chksumlen++;
+  }
+
+  return checksum ((uint16_t *) buf, chksumlen);
+}
