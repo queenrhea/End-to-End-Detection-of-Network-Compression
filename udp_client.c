2b46470d35a0a791325865d28656864ba000e373
queenrhea <earthtravel2010@gmail.com>
2020-04-12 14:05:32 -0700

    Update udp_client.c

--- udp_client.c
+++ udp_client.c
@@ -29,3 +29,58 @@ const struct sockaddr *to, socklen_t tolen);
 
 // set the buffer to 0 for all bytes
     memset(buffer, 0, PCKT_LEN);
+
+
+//------------------------------------------
+
+fillBuf(buf,size)
+register char *buf;
+register int size;
+{
+	register int i;
+
+	for ( i = 0; i < size; i++)
+		*buf++ = bufval;
+	bufval++;
+}
+
+
+
+fillBuf(mbuf.buf, BUFSIZE);
+
+	for ( i = 0; i < count; i++) {
+
+		printf("client msg no [%d]\n", i);
+
+		mbuf.count = i;
+
+		/* write msg to remote system
+		 * sock
+		 * buf
+		 * sizeof (union msg)
+		 * 0,
+		 * client
+		 * sizeof (client)
+		 */
+		if( rc = sendto(sock,&mbuf,sizeof (struct msgbuf),0,
+			 &client,sizeof (client)) < 0 ) {
+			/* buffers aren't available locally at the moment,
+			 * try again.
+			 */
+			if (errno == ENOBUFS)
+				continue;
+			perror("sending datagram");
+			exit(1);
+		}
+
+		fromlen = sizeof (struct sockaddr_in);
+
+		/* read acknowledgement from remote system
+		*/
+		if (recvfrom(sock, &ackvar, sizeof(long), 0, &from, &fromlen) < 0 ) {
+			printf("server error: errno %d\n",errno);
+			perror("reading datagram");
+			exit(1);
+		}
+    
+    //	struct sockaddr_in from;
