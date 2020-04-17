551b9ac1c57bf7086f12275d8d8961fa502a0071
Angie Preciado <42951261+apreciado98@users.noreply.github.com>
2020-04-16 21:27:35 -0700

    Update udp_client.c

--- udp_client.c
+++ udp_client.c
@@ -31,7 +31,7 @@ int main() {
     // Filling server information
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(DST_PORT);
-    servaddr.sin_addr.s_addr = inet_addr("192.168.1.19");
+    servaddr.sin_addr.s_addr = inet_addr("192.168.1.4");
 
     // set the buffer to 0 for all bytes
     memset(buffer, 0, MAXLINE);
@@ -55,22 +55,12 @@ int main() {
     printf("Server : %s\n", buffer);
     //}
 
-/*
+
     //******AFTER LOW ENTROPY DATA
 
-    //**********15 SEC. WAIT
-    int number_of_seconds = 15;
-    // Converting time into milli_seconds 
-    int milli_seconds = 1000 * number_of_seconds; 
-  
-    // Storing start time 
-    clock_t start_time = clock(); 
-  
+    //**********5 SEC. WAIT
     printf("Waiting...\n");
-    // looping till required time is not achieved 
-    while (clock() < start_time + milli_seconds) {
-        ; 
-    }
+    sleep(5);
     printf("Done waiting\n");
     //********* HIGH ENTROPY
 
@@ -79,47 +69,44 @@ int main() {
     FILE *fp;
 
 
-    fp = fopen("/dev/random", "r");
+    fp = fopen("/dev/urandom", "r");
 
     if(fp == NULL) {
-        printf("File not found.");
+        printf("File not found.\n");
         exit(0);
     }
 
     fread(&randval, sizeof(randval), 1, fp);
 
-    //write(buffer, &randval, sizeof(int));
-    //rewind(fp);
-
-    printf("Hi1");
+    printf("Hi1\n");
 
   
     //***********COPY BUFFER OF RANDOM BITS TO PAYLOAD
 
-    memcpy(&randval, &buffer, sizeof(buffer)); //copying src to dest, with sizeof(dest)
+    memcpy(&buffer, &randval, sizeof(buffer)); //copying src to dest, with sizeof(dest)
 
+    printf("Buffer: ");
     int y;
     int z = sizeof(buffer);
     for(y=0; y<z; y++){
-        printf("%d",buffer[z]);
+        printf("%u",buffer[z]);
     }
 
     //*****SEND THIS TO SERVER?
     int x;
-    for(x=0; x<2; x++){
+    //for(x=0; x<2; x++){
         sendto(sockfd, (char *)buffer, sizeof(buffer),
             0, (const struct sockaddr *) &servaddr,
             sizeof(servaddr));
-        printf("Hello message sent.\n");
-    }
+        printf("\nBuffer sent.\n");
+    //}
 
 
+    printf("Random value: %u\n", randval);
+    printf("Bye.\n");
 
-    printf("%u\n", randval);
-    printf("File has been successfully sent.\n");
-*/
 
     close(sockfd);
-    //fclose(fp);
+    fclose(fp);
     return 0;
 }
